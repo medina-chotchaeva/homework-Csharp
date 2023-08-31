c6af90b6159b57c967f75e72fc6ad5f8d6e00b96
Medina <chotchaevamedina@gmail.com>
2023-08-31 23:10:08 +0300

    Домашнее задание 3

---
+++ homework3/Program.cs
@@ -0,0 +1,56 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int GetExponent(int number, int extent)
+{
+    int result = 1;
+    for (int i = 0; i != extent; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+int num = GetExponent(3, 5);
+System.Console.WriteLine(num);
+
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetSumNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+
+    return sum;
+
+}
+
+int number = GetSumNum(1154);
+System.Console.WriteLine(number);
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+PrintArray(GenerateArray(8));
+
+int[] GenerateArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 99);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
